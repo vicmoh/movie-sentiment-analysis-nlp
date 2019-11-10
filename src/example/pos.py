43f2bfd9de6b0d350b158feff3753ca8f43bfe8d
Vicky Mohammad <vimoh1213@gmail.com>
2019-11-10 01:58:09 -0500

    [AUTO]

--- src/example/pos.py
+++ src/example/pos.py
@@ -1,8 +1,13 @@
+# This lib is for the POS-tagging and other
 import nltk
+
 # Download nltk dependencies
+# if you "nltk.download()" wihout param
+# it will open gui for downloading list of other
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
 # Test the nltk
-tokens = nltk.word_tokenize("Can you please buy me an Arizona Ice Tea? It's $0.99");
+tokens = nltk.word_tokenize(
+    "Can you please buy me an Arizona Ice Tea? It's $0.99")
 print("Part of Speech: ", nltk.pos_tag(tokens))
