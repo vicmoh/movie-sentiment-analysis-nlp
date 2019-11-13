cfda6af2983463173ab6530ade8e072149810a56
Vicky Mohammad <vimoh1213@gmail.com>
2019-11-13 18:09:05 -0500

    [AUTO]

--- src/main.py
+++ src/main.py
@@ -4,15 +4,17 @@ from matplotlib import pyplot as plot
 from sklearn import datasets
 from sklearn import svm
 
-# Open file and return list of string
-# of each lines
+
+def main():
+    print("Running script...")
 
 
 def readFile(filePath):
     lines = []
     if filePath == None:
-        return []
+        return lines
     file = open('my_text_file.txt')
     for line in file:
         lines.append(line)
+    file.close()
     return lines
