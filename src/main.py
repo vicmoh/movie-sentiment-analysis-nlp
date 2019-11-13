079c0ecfed1038602e27546ce51fce7fd6cb9244
Vicky Mohammad <vimoh1213@gmail.com>
2019-11-13 17:54:45 -0500

    [AUTO]

--- src/main.py
+++ src/main.py
@@ -4,10 +4,15 @@ from matplotlib import pyplot as plot
 from sklearn import datasets
 from sklearn import svm
 
-plot.xlabel('X Label')
-plot.ylabel('Y label')
-plot.title('Testing')
-plot.plot([1, 2, 3], [6, 7, 8], label = 'First')
-plot.plot([1, 2, 3], [5, 6, 7], label = 'Second')
-plot.legend() # plot.legend(['First', 'Second])
-plot.show()
+# Open file and return list of string
+# of each lines
+
+
+def readFile(filePath):
+    lines = []
+    if filePath == None:
+        return []
+    file = open('my_text_file.txt')
+    for line in file:
+        lines.append(line)
+    return lines
