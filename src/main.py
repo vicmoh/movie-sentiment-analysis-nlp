e37e5049f3a582579a8cc19b05faeafdb39719d9
Vicky Mohammad <vimoh1213@gmail.com>
2019-11-14 18:19:52 -0500

    [AUTO]

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+# Main file to run the program.
+
 # Import my files
 import sys
+from process import Process
 from util.util import Util
 from lib.pos import POSTagging
 # Import lib
@@ -8,13 +11,23 @@ from sklearn import datasets
 from matplotlib import pyplot as _matplot
 import scipy
 import numpy
+
 # For the debug statement
 _FILE_EXAMPLE = './assets/review_polarity/txt_sentoken/pos/cv000_29590.txt'
 _POS_FOLDER = './assets/review_polarity/txt_sentoken/pos/*.txt'
 
-# Main function
-def main():
-    print('Running script...')
-    Util.readAllFilesInFolder(_POS_FOLDER, numFiles=2)
-    POSTagging().runExample()
+
+class main:
+    def __init__(self):
+        super().__init__()
+        Process().run()
+
+    # Test function
+    @staticmethod
+    def testReadFile():
+        print('Running script...')
+        Util.readAllFilesInFolder(_POS_FOLDER, numFiles=2)
+        POSTagging().runExample()
+
+
 main()
